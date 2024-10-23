5466a7cc7adf8c7476d1c4676be83995b67258b5
Josh Marinacci <276938+joshmarinacci@users.noreply.github.com>
2024-10-22 17:37:14 -0700

    styling fixes

--- src/FileListView.tsx
+++ src/FileListView.tsx
@@ -1,10 +1,10 @@
 import {FileInfo, Site} from "./model.js";
 import {useState} from "react";
-import {ListView, StringRenderer, useChanged} from "rtds-react";
+import {ListItemRenderer, ListView, StringRenderer, useChanged} from "rtds-react";
 import {ObjMaybe} from "rtds-core";
 
-const renderFile: StringRenderer<typeof FileInfo> = (value: typeof FileInfo) => {
-    return `${value.get('fileName')}, ${value.get('fileType')}`
+const rend:ListItemRenderer<typeof FileInfo> = (value) => {
+    return <span className={'file-item'}>{value.get('fileName').get()}</span>
 }
 
 export function FileListView(props: { className: string, site: typeof Site }) {
@@ -12,7 +12,7 @@ export function FileListView(props: { className: string, site: typeof Site }) {
     useChanged(props.site)
     return <div className={'file-list'}>
         <ListView data={props.site.get('files')}
-                  renderer={renderFile}
+                  itemRenderer={rend}
                   selected={selected}
                   onSelect={(e) => {
                       props.site.set('selectedFile',e)
